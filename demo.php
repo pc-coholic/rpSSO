ff6d31206cbdee69ff162f0d6096fe43f22a468c
Martin Gross <martin@pc-coholic.de>
2012-09-21 12:58:51 +0200

    Updated examples

--- demo.php
+++ demo.php
@@ -3,7 +3,12 @@ require_once('rpSSO.class.php');
 
 $rpsso = new rpSSO('secretkey', 'http://your.rp.url.tld/');
 
-$rpSSO->auth('admin-123456', 'PASSWORD');
+if ($rpSSO->auth('admin-123456', 'PASSWORD')) {
+	echo("Login successfull");
+} else {
+	echo("Login failed");
+}
+echo '<br><br>';
 echo $rpSSO->bid();
 echo '<br><br>';
 echo $rpSSO->sid();
@@ -12,6 +17,6 @@ echo $rpSSO->create_challenge();
 echo '<br><br>';
 echo $rpSSO->get_sso();
 echo '<br><br>';
-echo $rpSSO->do_sso($rpSSO->create_challenge());
+#echo $rpSSO->do_sso($rpSSO->create_challenge());
 echo '<br><br>';
 ?>
